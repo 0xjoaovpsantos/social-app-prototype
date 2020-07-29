f979fa0a16224aca076d609f1414a723fa5d6fd1
Joao Victor Santos <joaovictorsantoscpv@gmail.com>
2020-07-29 13:50:39 -0300

    add: create redirect click in CardFriend component to Profile page

--- socialApp/src/components/CardFriend/index.tsx
+++ socialApp/src/components/CardFriend/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, TouchableHighlight } from 'react-native';
 import { Container, Info, Name, Status } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
-const CardFriend: React.FC = () => (
-  <Container>
-    <Image
-      style={{ width: 80, height: 80 }}
-      source={require('../../assets/james.jpeg')}
-    />
-    <Info>
-      <Name>Joao</Name>
-      <Status>Visto pela ultima vez as 13:30</Status>
-    </Info>
-  </Container>
-);
+const CardFriend: React.FC = () => {
+  const navigation = useNavigation();
+  return (
+    <TouchableHighlight onPress={() => navigation.navigate('Profile')}>
+      <Container>
+        <Image
+          style={{ width: 80, height: 80 }}
+          source={require('../../assets/james.jpeg')}
+        />
+        <Info>
+          <Name>Joao</Name>
+          <Status>Visto pela ultima vez as 13:30</Status>
+        </Info>
+      </Container>
+    </TouchableHighlight>
+  );
+};
 
 export default CardFriend;
--- socialApp/src/pages/ListFriends/index.tsx
+++ socialApp/src/pages/ListFriends/index.tsx
@@ -4,18 +4,20 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 import CardFriends from '../../components/CardFriend';
 
-const ListFriends: React.FC = () => (
-  <ScrollView>
-    <Text>Friends</Text>
-    <CardFriends />
-    <CardFriends />
-    <CardFriends />
-    <CardFriends />
-    <CardFriends />
-    <CardFriends />
-    <CardFriends />
-    <CardFriends />
-  </ScrollView>
-);
+const ListFriends: React.FC = () => {
+  return (
+    <ScrollView>
+      <Text>Friends</Text>
+      <CardFriends />
+      <CardFriends />
+      <CardFriends />
+      <CardFriends />
+      <CardFriends />
+      <CardFriends />
+      <CardFriends />
+      <CardFriends />
+    </ScrollView>
+  );
+};
 
 export default ListFriends;
